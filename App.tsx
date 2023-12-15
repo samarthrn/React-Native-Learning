d8a3170385310867f1dc1459bb82f3180e41f7eb
samarthrn <samarthnathile7670@gmail.com>
2023-12-15 14:43:24 +0530

    tutorial

--- App.tsx
+++ App.tsx
@@ -3,7 +3,7 @@ import { Text,View } from "react-native";
 const App =()=>{
   return(
     <View>
-        <Text style={{fontSize:30}} > Hello World </Text>
+        <Text style={{fontSize:30}} > Hello World this is my demo app </Text>
     </View>
   )
 }
